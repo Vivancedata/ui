11fcff640cd21fc3caa4cef820e86d5f388f0a38
Lorenzo <lorenzosca7@protonmail.ch>
2026-02-14 00:56:03 -0800

    Add src/components/container.tsx

---
+++ src/components/container.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../lib/utils";
+
+const containerVariants = cva("mx-auto w-full", {
+  variants: {
+    size: {
+      sm: "max-w-3xl",
+      default: "max-w-7xl",
+      lg: "max-w-screen-xl",
+      xl: "max-w-screen-2xl",
+      full: "max-w-full",
+    },
+    padding: {
+      none: "",
+      default: "px-4 sm:px-6 lg:px-8",
+      tight: "px-2 sm:px-4",
+      loose: "px-6 sm:px-8 lg:px-12",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+    padding: "default",
+  },
+});
+
+export interface ContainerProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof containerVariants> {}
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  ({ className, size, padding, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(containerVariants({ size, padding }), className)}
+      {...props}
+    />
+  )
+);
+Container.displayName = "Container";
+
+export { Container, containerVariants };
